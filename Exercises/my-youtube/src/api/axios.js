5d430e9989457bf07ac328de1cefa172e36b0007
Raed <raed_nb@hotmail.com>
2019-06-11 11:04:21 +0300

    settinguptheproject

--- Exercises/my-youtube/src/api/axios.js
+++ Exercises/my-youtube/src/api/axios.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 const KEY='AIzaSyDKrwXjzteN1vCf8yvr9ThMAST9szRX3LQ'
 
 export default axios.create({
-    baseURL:'https://www.googleapis.com/youtube/v3/',
+    baseURL:'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key='AIzaSyDKrwXjzteN1vCf8yvr9ThMAST9szRX3LQ'
+    &part=snippet,contentDetails,statistics,status
+',
     params:{
         part:'snippet',
         maxResults:20,
